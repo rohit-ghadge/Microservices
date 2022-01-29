d79bb50a20a479cc29503096646c38fcb6a88d71
ROHIT GHADGE <57706022+rohit-ghadge@users.noreply.github.com>
2022-01-29 16:17:30 +0530

    Add files via upload
    Citizen Service

---
+++ CitizenService/src/main/java/com/codedecode/microservices/CitizenService/Entity/Citizen.java
@@ -0,0 +1,48 @@
+package com.codedecode.microservices.CitizenService.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Citizen {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column(name = "vaccinationCenterId")
+	private int vaccinationCenterId;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getVaccinationCenterId() {
+		return vaccinationCenterId;
+	}
+
+	public void setVaccinationCenterId(int vaccinationCenterId) {
+		this.vaccinationCenterId = vaccinationCenterId;
+	}
+	
+	
+
+}
